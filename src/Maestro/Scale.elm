642e7d3347ae39459461fc9a1993f16cd264df82
tcr <tcr@ableton.com>
2017-04-06 13:57:36 +0200

    Document scale module

--- src/Maestro/Scale.elm
+++ src/Maestro/Scale.elm
@@ -1,12 +1,12 @@
 module Maestro.Scale exposing (Scale, Mode(..), scale)
 
-{-| This library fills a bunch of important niches in Elm. A `Maybe` can help
-you with optional arguments, error handling, and records with optional fields.
+{-| This module provides types and functions to create and
+manipulate scales.
 
 # Types
 @docs Scale, Mode
 
-# Common Helpers
+# Scales manipulation
 @docs scale
 
 -}
@@ -16,32 +16,32 @@ import Maestro.Note exposing (Note, newNote)
 import Maestro.Interval exposing (Interval(..), addInterval, majorIntervals, minorIntervals)
 
 
-{-|
+{-| Scale represents a list of tones composing it
 -}
 type alias Scale =
     List Tone
 
 
-{-|
+{-| Mode represents the mode of a scale.
 -}
 type Mode
     = Major
     | Minor
 
 
-{-|
--}
-modeToIntervals : Mode -> List Interval
-modeToIntervals mode =
-    case mode of
-        Major ->
-            majorIntervals
+{-| Given a Tone and a Mode, generates the tones
+composing a scale.
 
-        Minor ->
-            minorIntervals
-
-
-{-|
+    (==)
+      scale (newTone C Major) Major
+      [ { key = C, accidental = Natural }
+      , { key = D, accidental = Natural }
+      , { key = E, accidental = Natural }
+      , { key = F, accidental = Natural }
+      , { key = G, accidental = Natural }
+      , { key = A, accidental = Natural }
+      , { key = B, accidental = Natural }
+      ]
 -}
 scale : Tone -> Mode -> List Tone
 scale tone mode =
@@ -50,3 +50,13 @@ scale tone mode =
             newNote tone.key tone.adjustment 3
     in
         List.map (\i -> (addInterval placeholderNote i).tone) (modeToIntervals mode)
+
+
+modeToIntervals : Mode -> List Interval
+modeToIntervals mode =
+    case mode of
+        Major ->
+            majorIntervals
+
+        Minor ->
+            minorIntervals
