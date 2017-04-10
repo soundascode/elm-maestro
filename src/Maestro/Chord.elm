f6774b624f785ba051cf1d3b3f6bbb7d62492161
oleiade <tcrevon@gmail.com>
2017-04-10 21:13:35 +0200

    Add augmented, diminished and seventh chords

--- src/Maestro/Chord.elm
+++ src/Maestro/Chord.elm
@@ -27,6 +27,17 @@ type alias Chord =
 type Quality
     = MajorTriad
     | MinorTriad
+    | AugmentedTriad
+    | DiminishedTriad
+    | Seventh
+    | MajorSeventh
+    | MinorMajorSeventh
+    | MinorSeventh
+    | AugmentedMajorSeventh
+    | AugmentedSeventh
+    | HalfDiminishedSeventh
+    | DiminishedSeventh
+    | SeventhFlatFive
 
 
 {-| Given a Tone and a Quality, generates the tones
@@ -57,18 +68,148 @@ qualityToIntervals quality =
         MinorTriad ->
             minorTriad
 
+        AugmentedTriad ->
+            augmentedTriad
+
+        DiminishedTriad ->
+            diminishedTriad
+
+        Seventh ->
+            seventh
+
+        MajorSeventh ->
+            majorSeventh
+
+        MinorMajorSeventh ->
+            minorMajorSeventh
+
+        MinorSeventh ->
+            minorSeventh
+
+        AugmentedMajorSeventh ->
+            augmentedMajorSeventh
+
+        AugmentedSeventh ->
+            augmentedSeventh
+
+        HalfDiminishedSeventh ->
+            halfDiminishedSeventh
+
+        DiminishedSeventh ->
+            diminishedSeventh
+
+        SeventhFlatFive ->
+            seventhFlatFive
+
 
 majorTriad : List Interval
 majorTriad =
-    [ PerfectUnison
-    , MajorThird
-    , PerfectFifth
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MajorThird
+    , Maestro.Interval.PerfectFifth
     ]
 
 
 minorTriad : List Interval
 minorTriad =
-    [ PerfectUnison
-    , MinorThird
-    , PerfectFifth
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MinorThird
+    , Maestro.Interval.PerfectFifth
+    ]
+
+
+augmentedTriad : List Interval
+augmentedTriad =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MajorThird
+    , Maestro.Interval.AugmentedFifth
+    ]
+
+
+diminishedTriad : List Interval
+diminishedTriad =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MinorThird
+    , Maestro.Interval.DiminishedFifth
+    ]
+
+
+seventh : List Interval
+seventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MajorThird
+    , Maestro.Interval.PerfectFifth
+    , Maestro.Interval.MinorSeventh
+    ]
+
+
+majorSeventh : List Interval
+majorSeventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MajorThird
+    , Maestro.Interval.PerfectFifth
+    , Maestro.Interval.MajorSeventh
+    ]
+
+
+minorMajorSeventh : List Interval
+minorMajorSeventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MinorThird
+    , Maestro.Interval.PerfectFifth
+    , Maestro.Interval.MajorSeventh
+    ]
+
+
+minorSeventh : List Interval
+minorSeventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MinorThird
+    , Maestro.Interval.PerfectFifth
+    , Maestro.Interval.MinorSeventh
+    ]
+
+
+augmentedMajorSeventh : List Interval
+augmentedMajorSeventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MajorThird
+    , Maestro.Interval.AugmentedFifth
+    , Maestro.Interval.MajorSeventh
+    ]
+
+
+augmentedSeventh : List Interval
+augmentedSeventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MajorThird
+    , Maestro.Interval.AugmentedFifth
+    , Maestro.Interval.MinorSeventh
+    ]
+
+
+halfDiminishedSeventh : List Interval
+halfDiminishedSeventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MinorThird
+    , Maestro.Interval.DiminishedFifth
+    , Maestro.Interval.MinorSeventh
+    ]
+
+
+diminishedSeventh : List Interval
+diminishedSeventh =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MinorThird
+    , Maestro.Interval.DiminishedFifth
+    , Maestro.Interval.DiminishedSeventh
+    ]
+
+
+seventhFlatFive : List Interval
+seventhFlatFive =
+    [ Maestro.Interval.PerfectUnison
+    , Maestro.Interval.MajorThird
+    , Maestro.Interval.DiminishedFifth
+    , Maestro.Interval.MinorSeventh
     ]
